4afaa60eb748843eac5f106a53d8450882636961
TeoResende <resendenetto@yahoo.com.br>
2022-06-30 20:46:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//window.document.write("SENAI 22")
+nome = prompt("Olá!, Qual o seu nome?");
+
+if (nome == "SENAI") {
+	window.document.write("É isso ai!");
+} else {
+	document.write("Bem vindo ao JS, " + nome + " Espero que goste!");
+}
